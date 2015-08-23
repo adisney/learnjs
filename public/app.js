72ae3921888bac4eadfe905b660e312e195f8d18
Alex Disney <alexdisney@gmail.com>
2015-08-23 18:03:07 -0500

    finished up through chapter 3

---
+++ public/app.js
@@ -0,0 +1,90 @@
+"use strict"
+
+var learnjs = {};
+
+learnjs.problems = [
+  {
+    description: "What is truth?",
+    code: "function problem() { return __; }"
+  },
+  {
+    description: "Simple Math",
+    code: "function problem() { return 42 === 6 * __; }"
+  }
+];
+
+learnjs.applyObject = function(obj, elem) {
+  for (var key in obj) {
+    elem.find('.' + key).text(obj[key]);
+  }
+};
+
+learnjs.flashElement = function(elem, content) {
+  elem.fadeOut('fast', function() {
+    elem.html(content);
+    elem.fadeIn();
+  });
+}
+
+learnjs.template = function(name) {
+  return $('.templates .' + name).clone();
+}
+
+learnjs.buildCorrectFlash = function(problemNum) {
+  var correctFlash = learnjs.template('correct-flash');
+  var link = correctFlash.find('a');
+  if(problemNum < learnjs.problems.length) {
+    link.attr('href', '#problem-' + (problemNum + 1));
+  } else {
+    link.attr('href', '');
+    link.text("You're Finished!");
+  }
+  return correctFlash;
+}
+
+learnjs.problemView = function(data) {
+  var problemNumber = parseInt(data, 10);
+  var view = $('.templates .problem-view').clone();
+  var problemData = learnjs.problems[problemNumber - 1];
+  var resultFlash = view.find('.result');
+
+  function checkAnswer() {
+    var answer = view.find('.answer').val();
+    var test = problemData.code.replace('__', answer) + "; problem();";
+    return eval(test);
+  }
+
+  function checkAnswerClick() {
+    if (checkAnswer()) {
+      var flashContent = learnjs.buildCorrectFlash(problemNumber);
+      learnjs.flashElement(resultFlash, flashContent);
+    } else {
+      learnjs.flashElement(resultFlash, 'Incorrect!');
+    }
+    return false;
+  }
+
+  view.find('.check-btn').click(checkAnswerClick);
+  view.find('.title').text('Problem #' + problemNumber);
+  learnjs.applyObject(learnjs.problems[problemNumber - 1], view);
+  return view;
+};
+
+learnjs.showView = function(hash) {
+  var routes = {
+    "#problem": learnjs.problemView 
+  };
+  var hashParts = hash.split('-');
+  var viewFn = routes[hashParts[0]]
+  if (viewFn) {
+    $('.view-container').empty().append(viewFn(hashParts[1]));
+  }
+};
+
+learnjs.appOnReady = function() {
+  learnjs.showView(window.location.hash);
+  window.onhashchange = function() {
+    learnjs.showView(window.location.hash);
+  }
+  learnjs.showView(window.location.hash);
+};
