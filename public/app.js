2a62abd1f480275a3398037aadecae1a6589c6b9
Alex Disney <alexdisney@gmail.com>
2016-02-28 17:43:43 -0600

    Chapter 6 without adding a view for the popularAnswers

--- public/app.js
+++ public/app.js
@@ -99,6 +99,19 @@ learnjs.problemView = function(data) {
   return view;
 };
 
+learnjs.popularAnswers = function(problemId) {
+  return learnjs.identity.then(function() {
+    var lambda = new AWS.Lambda();
+    var params = {
+      FunctionName: 'learnjs_popularAnswers',
+      Payload: JSON.stringify({problemNumber: problemId})
+    };
+    return learnjs.sendAwsRequest(lambda.invoke(params), function() {
+      return learnjs.popularAnswers(problemId);
+    });
+  });
+}
+
 learnjs.showView = function(hash) {
   var routes = {
     "#problem": learnjs.problemView,
